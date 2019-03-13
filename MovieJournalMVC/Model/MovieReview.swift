bcb324ca5837f66f40b20175315fd8616dc39e05
Lauren Small <lauren.small@detroitlabs.com>
2019-03-12 21:42:54 -0400

    create a movie review model (class)

---
+++ MovieJournalMVC/Model/MovieReview.swift
@@ -0,0 +1,19 @@
+//
+//  MovieReview.swift
+//  MovieJournalMVC
+//
+//  Created by Lauren Small on 3/12/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import Foundation
+
+class MovieReview {
+    var title: String = ""
+    var review: String = ""
+    
+    init(title: String, review: String) {
+        self.title = title
+        self.review = review
+    }
+}
