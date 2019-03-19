ab9656787f324a52c440789666a8597de1bdd0a3
Lauren Small <lauren.small@detroitlabs.com>
2019-03-19 10:54:35 -0400

    add segues to show and add entries

---
+++ MovieJournalMVC/Controller/EntryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryDetailViewController.swift
+//  MovieJournalMVC
+//
+//  Created by Lauren Small on 3/19/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
