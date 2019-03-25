fcf240708c8523a669f665e3de3f295baef4ce28
Lauren Small <lauren.small@detroitlabs.com>
2019-03-25 18:39:13 -0400

    Removing old commented out code from the Store class

--- MovieJournalMVC/Model/Store.swift
+++ MovieJournalMVC/Model/Store.swift
@@ -6,19 +6,6 @@
 //  Copyright © 2019 Lauren Small. All rights reserved.
 //
 
-//import Foundation
-//
-//class Store {
-//    static let shared: Store = Store()
-//
-//    var movieReviews: [MovieReview]
-//
-//    private init(){
-//        movieReviews = [MovieReview]()
-//    }
-//}
-
-
 import Foundation
 
 class Store {
