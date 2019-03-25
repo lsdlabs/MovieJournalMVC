3a259ea8ba516027571ac8aa95224025bf7eddfc
Lauren Small <lauren.small@detroitlabs.com>
2019-03-25 13:51:27 -0400

    Adding enum for segue identifier and guarding against segue identifier

---
+++ MovieJournalMVC/Controller/SegueIdentifier.swift
@@ -0,0 +1,13 @@
+//
+//  SegueIdentifier.swift
+//  MovieJournalMVC
+//
+//  Created by Lauren Small on 3/25/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import Foundation
+
+enum SegueIndentifier: String {
+    case showEntry = "showEntry"
+}
